b1908599e75ade8f4182fe8d6b5e10648340ce12
Carlos Zuluaga Mora <carloszulmor@unisabana.edu.co>
2023-12-15 15:21:17 -0500

    Cambia botones, agrega asignacion

--- src/App.js
+++ src/App.js
@@ -3,28 +3,54 @@ import './App.css';
 import Boton from './Componentes/Boton';
 import Contador from './Componentes/Contador';
 
-
 function App() {
-
   const [cuenta, setCuenta] = useState(0);
+  const [valorInput, setValorInput] = useState('');
 
-  const handleClick = () =>{
+  const sumar = () => {
     setCuenta(cuenta + 1);
-  }
+  };
 
-  const resetContador = () =>{
+  const restar = () => {
+    setCuenta(cuenta - 1);
+  };
+
+  const asignarContador = () => {
+    // Convertir el valor del input a un número antes de asignarlo
+    const nuevoValor = parseInt(valorInput, 10);
+    if (!isNaN(nuevoValor)) {
+      setCuenta(nuevoValor);
+      setValorInput("");
+    }else{
+      
+    }
+  };
+
+  const resetContador = () => {
     setCuenta(0);
-  }
+  };
+
+  const manejarInputChange = (e) => {
+    setValorInput(e.target.value);
+  };
 
   return (
     <div className="App">
       <h1>¡Hola, mundo!</h1>
       <div className="contenedor">
-        <Contador numClicks = {cuenta}/>
+        <Contador numClicks={cuenta} />
         <div className="botones">
-        <Boton texto = "Click" tipo = "Click" manejarClick={handleClick}/>
-        <Boton texto = "Reinicio" tipo = "Reinicio" manejarClick={resetContador}/>
+          <Boton texto="Suma" tipo="Suma" manejarClick={sumar} />
+          <Boton texto="Resta" tipo="Resta" manejarClick={restar} />
+          <Boton texto="Asignación" tipo="Asignacion" manejarClick={asignarContador} />
+          <Boton texto="Reinicio" tipo="Reinicio" manejarClick={resetContador} />
         </div>
+        <input
+          type="number"
+          value={valorInput}
+          onChange={manejarInputChange}
+          placeholder="Ingresa un valor"
+        />
       </div>
     </div>
   );
--- src/Componentes/Boton.jsx
+++ src/Componentes/Boton.jsx
@@ -3,7 +3,7 @@ import "../Stylesheets/Boton.css";
 
 function Boton( {texto, tipo, manejarClick} ) {
   return (
-    <button className = {tipo === "Click" ? "Click" : "Reinicio"} onClick={manejarClick}>
+    <button className={tipo === "Suma" ? "Suma" : tipo === "Resta" ? "Resta" : tipo === "Asignacion" ? "Asignacion" : tipo === "Reinicio" ? "Reinicio" : ""} onClick={manejarClick}>
       {texto}
     </button>
   );
